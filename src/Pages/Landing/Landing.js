7f95257b4d924985a16beee6c4dccfa19a934a85
MuhammadAhmadEjaz <ahmadejaz343565@gmail.com>
2022-10-09 15:07:51 +0500

    Created button Component

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './Landing.scss';
+import React from "react";
+import "./Landing.scss";
+import ColoredButton from "../../Components/Buttons/ColoredButton/ColoredButton";
 
 function Landing(props) {
-    return (
-        <div className="landing">
-            landing
-        </div>
-    );
+  return (
+    <div className="landing">
+      <div className="test">
+        <ColoredButton text={'Sign Up'}></ColoredButton>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
